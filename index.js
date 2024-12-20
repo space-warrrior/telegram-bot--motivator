19e20470667dd070a38e1616c6f8ba0239f4be4d
7FOX7 <kheeugene@gmail.com>
2024-12-19 19:00:51 -0500

    Added the start command

--- index.js
+++ index.js
@@ -3,7 +3,33 @@ const {Bot} = require("grammy")
 
 const bot = new Bot(process.env.API_KEY)
 
+// The text will be shown when the user starts the bot: 
+const startHTML = `
+<b>What can this bot do?</b>
 
-console.log(process.env.API_KEY)
+Training Practice Bot helps you practice and learn about new
+Telegram Bot features in an easy and convenient way
+
+About the Author: <a href="https://github.com/7FOX7">https://github.com/7FOX7</a>
+
+Contact <a href="mailto:kheeugene@gmail.com">kheeugene@gmail.com</a> should you have any questions and concerns 
+`
+
+// Show the list of commands: 
+bot.api.setMyCommands([
+   {
+      command: "start", 
+      description: "Start the bot"
+   }
+])
+
+bot.command("start", ctx => {
+   ctx.reply(startHTML, {
+      parse_mode: "HTML", 
+      link_preview_options: {
+         is_disabled: true
+      }
+   })
+})
 
 bot.start()
