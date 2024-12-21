d29c718207609739eaaac6ff98e4fd73ed3918bc
7FOX7 <kheeugene@gmail.com>
2024-12-20 21:13:53 -0500

    Added a cron job that is scheduled to send quotes to the users every day at the specified time

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require("dotenv").config()
-const { hydrate } = require("@grammyjs/hydrate")
-const {Bot, InlineKeyboard, GrammyError, HttpError} = require("grammy")
 const {createClient} = require("@supabase/supabase-js")
+const {Bot, InlineKeyboard, GrammyError, HttpError} = require("grammy")
+const { hydrate } = require("@grammyjs/hydrate")
+const {Cron} = require("croner")
 
 const client = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY, {
    db: {
@@ -12,6 +13,7 @@ const client = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY, 
 const bot = new Bot(process.env.API_KEY)
 bot.use(hydrate())   // will hydrate the state on every update
 
+// The selected category and time will be stored in these variables:
 let selectedCategory = ""
 let selectedTime = ""
 
@@ -26,6 +28,16 @@ About the Author: <a href="https://github.com/7FOX7">https://github.com/7FOX7</a
 
 Contact <a href="mailto:kheeugene@gmail.com">kheeugene@gmail.com</a> should you have any questions and concerns 
 `
+
+// The function to get the quote in HTML format:
+const getQuoteHTML = (firstName, content, author) => {
+   return `
+      👋 Hi, ${firstName} ! Here is your daily quote:
+      <blockquote>${content}</blockquote>
+
+      by <i>${author}</i>
+   `
+}
 
 // The categories will be used in the inline keyboard:
 const categories = [
@@ -61,6 +73,7 @@ bot.api.setMyCommands([
    }
 ])
 
+// Show the start message when user starts the bot:
 bot.command("start", ctx => {
    ctx.reply(startHTML, {
       parse_mode: "HTML", 
@@ -77,6 +90,7 @@ bot.command("select_category", ctx => {
    })
 })
 
+// Handle the callback query when user selects a category:
 bot.callbackQuery(/category[1-3]/, ctx => {
    selectedCategory = ctx.callbackQuery.data
    ctx.editMessageText("At what time would you like to receive quotes?", {
@@ -90,28 +104,40 @@ bot.callbackQuery(/category[1-3]/, ctx => {
    )
 })
 
+// Handle the callback query when user selects a time:
 bot.callbackQuery(/8|12|18/, async function(ctx) {
    try {
-      const {first_name, id} = ctx.from
-      selectedTime = ctx.callbackQuery.data
+      const {first_name, id} = ctx.from      // Get the user's first name and telegram id
+      selectedTime = ctx.callbackQuery.data  // Get the selected time
       let categoryId = Number(selectedCategory.split("category")[1]); 
       await client.from("users").insert(
          {
             firstName: first_name, 
             telegramId: id, 
-            categoryId, 
+            categoryId: categoryId, 
             time: selectedTime
          }
       )
+      // Get all the users from the database:
+      const users = (await client.from("users").select("*")).data
+      if(users.length === 0) throw new Error("No users selected")
+      // Send the quotes to all the users:
+      users.forEach(async (user) => {
+         await sendQuotes(user)
+      })
+
+      // Show the message to the user after successfully setting up the quotes: 
       await ctx.editMessageText(`Got it! You will be getting quotes about *${categories[categoryId -1 ].text}* at *${selectedTime}:00* every day!`, {
          parse_mode: "Markdown"
       })
    }
    catch(err) {
+      ctx.reply("😨 Ooops! Something went wrong. Please try again later.")
       console.error("Something went wrong: ", err)
    }
 })
 
+// Handle the callback query when user selects the back button:
 bot.callbackQuery("back", ctx => {
    ctx.editMessageText("What category would you like to choose?", {
       reply_markup: InlineKeyboard.from(inlineButtons)
@@ -132,3 +158,16 @@ bot.catch(err => {
 })
 
 bot.start()
+
+// UTILS: 
+async function sendQuotes(user) {
+   const {firstName, telegramId, categoryId, time} = user
+   const job = new Cron(`* ${time} * * *`, async () => {
+      const {content, author} = (await client.from("quotes").select("*").eq('categoryId', categoryId)).data[0]
+      if(!content || !author) throw new Error("No content or author")
+      await bot.api.sendMessage(telegramId, getQuoteHTML(firstName, content, author), {
+         parse_mode: "HTML"
+      })
+   })
+   job.schedule()
+}
